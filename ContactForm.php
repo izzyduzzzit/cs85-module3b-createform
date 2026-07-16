a45b6f7089a46a9aae836e00c2d1d727c9349e83
Izarra Villareal <izarravill@gmail.com>
2026-07-16 11:00:55 -0700

    Added sticky form behavior, email address validation, form errormessage variable, & htmlspecialchars XSS protection around form action

--- ContactForm.php
+++ ContactForm.php
@@ -1,45 +1,58 @@
 <!DOCTYPE HTML>
 <html>
+
 <head>
     <title>Contact Izarra</title>
     <link rel="stylesheet" href="style/style.css">
 </head>
+
 <body>
 
 <?php
-// Variable for whether the form upon submission passes validation and is considered submitted
+
+// Variable for holding whether the form passes validation
 $submitted = false;
 
-// Variables that will hold the data entered by form user
+// Variable for holding form validation errors
+$errorMessage = "";
+
+// Variables that will hold the data entered by the user
 $name = "";
 $email = "";
 $topic = "";
 $message = "";
 
-// PHP logic to check that the form method is post upon submit
+// Check whether the form was submitted using POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // htmlspecialchars used to prevent XSS (cross-site scripting) attacks via form input
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $topic = htmlspecialchars($_POST['topic']);
-    $message = htmlspecialchars($_POST['message']);
+    // Sanitize user input to help prevent XSS attacks
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $topic = htmlspecialchars(trim($_POST['topic']));
+    $message = htmlspecialchars(trim($_POST['message']));
 
-    // PHP input validation logic
+    // Validate all inputs
     if (
         !empty($name) &&
-        !empty($email) &&
+		
+		// Added email format validation using filter_var
+        filter_var($email, FILTER_VALIDATE_EMAIL) &&
         !empty($topic) &&
         !empty($message)
-    )	{
+    ) {
         $submitted = true;
+    } else {
+		
+		// Error message if email validation does not pass
+        $errorMessage = "Please complete all fields and enter a valid email address.";
     }
 }
 ?>
 
 <?php
-// PHP form submission handling logic
-if ($submitted == true) {
+
+// Display a thank you message if the form is submitted properly
+if ($submitted) {
 ?>
 <h2>Thank you, <?php echo $name; ?>!</h2>
 <p>
@@ -50,37 +63,44 @@ if ($submitted == true) {
     We'll get back to you at
     <?php echo $email; ?>.
 </p>
+
 <?php
 } else {
 ?>
 
 <h2>Contact Izarra</h2>
-<form action="" method="POST">
+
+<?php
+
+// Display the error message if form validation does not pass
+if (!empty($errorMessage)) {
+?>
+<p><strong><?php echo $errorMessage; ?></strong></p>
+
+<?php
+}
+?>
+
+<!-- Added htmlspecialchars as a security best practice when posting the form back to itself -->
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
     <label for="name">
         Full Name:
     </label>
-    <input type="text" id="name" name="name" required>
-    <br>
-	<br>
+	
+	<!-- Added sticky form behavior using value= parameter on form inputs -->
+    <input type="text" id="name" name="name" value="<?php echo $name; ?>" required>
     <label for="email">
         Email Address:
     </label>
-    <input type="email" id="email" name="email" required>
-    <br>
-	<br>
+    <input type="email" id="email" name="email" value="<?php echo $email; ?>" required>
     <label for="topic">
         Topic of Message:
     </label>
-    <input type="text" id="topic" name="topic" required>
-    <br>
-	<br>
+    <input type="text" id="topic" name="topic" value="<?php echo $topic; ?>" required>
     <label for="message">
         Message:
     </label>
-    <br>
-    <textarea id="message" name="message" rows="6" cols="50" minlength="50" maxlength="150" required></textarea>
-    <br>
-	<br>
+    <textarea id="message" name="message" rows="6" minlength="50" maxlength="150" required><?php echo $message; ?></textarea>
     <input type="submit" value="Submit">
 </form>
 
